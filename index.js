5bfb57ff87c1b910fc7772b124adb87624252d86
xtrica <xtrica.com@gmail.com>
2017-12-19 01:36:53 -0500

    v1.0.1513665413

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * xtrica-notify v1.0.1512929300 (https://xtrica.com)
+ * xtrica-notify v1.0.1513665413 (https://xtrica.com)
  * Copyright 2017 (or 2150?) Xtrica
  * Licensed under MIT
  */
@@ -11,22 +11,22 @@ this.timeout=null
 this.notifyActive=!1
 this.notify=function(html,classes,duration){let vm=this
 return new Promise(function(resolve,reject){vm.notifyActive=!0
-if(vm.timeout){clearTimeout(vm.timeout)}
-if(vm.element){vm.element.style.opacity=1
+if(!!vm&&!!vm.timeout){clearTimeout(vm.timeout)}
+if(!!vm&&!!vm.element){vm.element.style.opacity=1
 vm.element.style.display='inline-block'
 if(classes){vm.element.className=classes}
 vm.element.innerHTML=html
 vm.remaining=duration
-vm.timeout=setTimeout(()=>{vm.element.style.opacity=0
-setTimeout(()=>{vm.element.style.display='none'
+vm.timeout=setTimeout(()=>{if(!!vm&&!!vm.element){vm.element.style.opacity=0
+setTimeout(()=>{if(!!vm&&vm.element){vm.element.style.display='none'
 vm.element.innerHTML=''
-vm.notifyActive=!1
-resolve()},300)},parseInt(duration))}else{vm.notifyActive=!1
+vm.notifyActive=!1}
+resolve()},300)}else{resolve()}},parseInt(duration))}else{if(!!vm){vm.notifyActive=!1}
 resolve()}})}
 this.clear=()=>{let vm=this
-return new Promise(function(resolve,reject){if(vm.timeout){clearTimeout(vm.timeout)}
-if(vm.element){vm.element.style.opacity=0
+return new Promise(function(resolve,reject){if(!!vm&&!!vm.timeout){clearTimeout(vm.timeout)}
+if(!!vm&&!!vm.element){vm.element.style.opacity=0
 setTimeout(()=>{resolve()},300)}else{resolve()}})}
 this.cleanup=()=>{let vm=this
-vm.clear().then(()=>{if(vm.element){vm.element.parentElement.removeChild(vm.element)
+vm.clear().then(()=>{if(!!vm&&!!vm.element){vm.element.parentElement.removeChild(vm.element)
 vm.element=null}})}}
